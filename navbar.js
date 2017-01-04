48c51fff5a91015e27fedc7106941e9a9ab591e0
codea_mac_04 <codea_mac_04@gmail.com>
2017-01-04 16:26:31 -0600

    Funamentos web martes 1

--- navbar.js
+++ navbar.js
@@ -1,4 +1,36 @@
-$(window).on("scroll", function () {
-  // Aquí deberás escribir la lógica que modificará la barra
+// var divs = $('.top-nav'),
+//     limit = 35;  /* scrolltop value when opacity should be 0 */
+
+// $(window).on('scroll', function() {
+//    var st = $(this).scrollTop();
+
+//    /* avoid unnecessary call to jQuery function */
+//    if (st <= limit) {
+//       divs.css({ 'opacity' : (1 - st/limit) });
+//    }
+// });
+
+$(function() {
+  $(window).scroll(function(){
+    var scrollTop = $(window).scrollTop();
+    if(scrollTop != 0)
+      $('nav').stop().animate({'opacity':'0.5'},400);
+    else  
+      $('nav').stop().animate({'opacity':'1'},400);
+  });
   
+  $('nav').hover(
+    function (e) {
+      var scrollTop = $(window).scrollTop();
+      if(scrollTop != 0){
+        $('nav').stop().animate({'opacity':'1'},400);
+      }
+    },
+    function (e) {
+      var scrollTop = $(window).scrollTop();
+      if(scrollTop != 0){
+        $('nav').stop().animate({'opacity':'0.5'},400);
+      }
+    }
+  );
 });
